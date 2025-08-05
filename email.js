565131d7e093f19f20047102547b3192de434b6e
ragini377 <ragini.panwar377@gmail.com>
2025-08-05 15:58:17 +0530

    Update email.js

--- email.js
+++ email.js
@@ -1,13 +1,54 @@
+
+
+
 window.addEventListener("DOMContentLoaded", function () {
     emailjs.init("VoSKZ7mJb4nN_xW7j");
 
+  
+  const addItemMsg = document.getElementById("additemMsgshow");
+  const bookingConfirm = document.getElementById("booking-confirm");
     const form = document.getElementById("contact-form");
+     const table = document.getElementById("serviceTable");
+     document.querySelectorAll(".addServiceBtn").forEach(function (btn) {
+  btn.addEventListener("click", function () {
+    if (table.querySelector("td")) {
+      bookingConfirm.innerHTML = "";
+    }
+  });
+});
+
+
     form.addEventListener("submit", function (event) {
       event.preventDefault();
+        if (!form.checkValidity()) {
+      form.reportValidity(); // Show native browser validation
+      return;
+    }
+       const isVisible = window.getComputedStyle(addItemMsg).display === "block";
 
+
+    if (isVisible) {
+      bookingConfirm.innerHTML = " &#x2296; Add Services For Booking";
+      bookingConfirm.style.color = "red";
+      return; // Stop form submission
+    }
+    
+    const hasTD = table.querySelector("td") ? true : false;
+    if (!hasTD) {
+      bookingConfirm.innerHTML = "&#x2296; No services added to the table.";
+      bookingConfirm.style.color = "red";
+      return;
+    }
+     else {
+  // Clear message if valid
+  bookingConfirm.innerHTML = "";
+       }
+    
+  
       emailjs.sendForm("service_99nduch", "template_nihvsxt", form)
         .then(() => {
           document.getElementById("booking-confirm").innerHTML = " “Thank you For Booking the Service.We contact you soon!”"
+          bookingConfirm.style.color = "green";
           // alert("✅ Email sent to user.");
           form.reset();
         })
@@ -17,5 +58,3 @@ window.addEventListener("DOMContentLoaded", function () {
         });
     });
   });
-
-
